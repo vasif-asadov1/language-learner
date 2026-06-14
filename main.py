021a01e336ed10a4e4fc9e89672d9127cff0c086
vasif-asadov1 <vasif.asadov.1828@gmail.com>
2026-06-14 22:43:55 +0300

    Update release workflow and finalize v1.1.0

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ class LanguageLearnerUI(QMainWindow):
         super().__init__()
         
         # --- APP VERSION ---
-        self.current_version = "v1.0.6" 
+        self.current_version = "v1.1.0" 
         
         self.session_time = datetime.now().strftime("%Y%m%d_%H%M%S")
         self.db_name = os.path.join(APP_DIR, f"session_{self.session_time}.db")
